6319c25745972905515d4bb5d73b7a12d5e2a115
Hewitt Jung <hwttjung@gmail.com>
2022-05-30 00:14:57 +0900

    second commit

--- shorten/views.py
+++ shorten/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from shorten.models import User
 # Create your views here.
+
+def index(request) :
+    user = User.objects.filter(username="admin").first()
+    email = user.email if user else "Anoymous User!"
+    print(email)
+    if request.user.is_authenticated is False:
+        email = "Anonymous User"
+    print(email)
+    return render(request, "base.html", {"welcome_msg": f"Hello {email}"})
+
+
+def redirect_test(request) :
+    print("Go Redirect!")
+    return redirect("index")
